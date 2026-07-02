6bc115895132c7a459415aa9858e21c9890adf1c
Diego Sánchez <deknoscoot03@gmail.com>
2026-07-02 19:26:52 +0200

    solucionar import os

--- backend/database.py
+++ backend/database.py
@@ -1,14 +1,10 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 # 1. Definimos dónde se guardará la base de datos (en un archivo local llamado suplementos.db)
-SQLALCHEMY_DATABASE_URL = "sqlite:///./suplementos.db"
-
-# 2. Creamos el "motor" que se conecta a la base de datos
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, 
-    connect_args={"check_same_thread": False} # Esto es un requisito específico de SQLite
-)
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 # 3. Creamos una "fábrica de sesiones" (las sesiones son como carritos de la compra para sacar/meter datos)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
